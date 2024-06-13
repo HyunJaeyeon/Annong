a4a84a102be538a6c74faf1e9182ec721f418895
HyunJaeyeon <bomul0109@gmail.com>
2024-06-13 20:08:16 +0900

    first commit

---
+++ Annong/AnnongApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnnongApp.swift
+//  Annong
+//
+//  Created by Hyun Jaeyeon on 6/13/24.
+//
+
+import SwiftUI
+
+@main
+struct AnnongApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Annong/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Annong
+//
+//  Created by Hyun Jaeyeon on 6/13/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}

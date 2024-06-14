58233fec49e9b4fb6f23a4f34e2b272c5034790c
HyunJaeyeon <bomul0109@gmail.com>
2024-06-14 17:54:04 +0900

    Feat: Message List 구현

--- Annong/MessageBoxView.swift
+++ Annong/MessageBoxView.swift
@@ -1,8 +1,24 @@
-//
-//  MessageBoxView.swift
-//  Annong
-//
-//  Created by Hyun Jaeyeon on 6/14/24.
-//
+import SwiftUI
+import SwiftData
 
-import Foundation
+struct MessageBoxView: View {
+    
+    var body: some View {
+        VStack(alignment: .leading, spacing: 0) {
+            Text("💎kanto00의 우편함")
+                .font(.title)
+                .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                .foregroundStyle(.accent)
+                .padding(.leading)
+            
+            List {
+                Text("나 스티브잡스인데, 이 앱 좋다")
+                Text("나 조너선 아이브인데, 이 앱 이쁘다")
+            }
+        }
+    }
+}
+#Preview {
+    MessageBoxView()
+        .preferredColorScheme(.dark)
+}

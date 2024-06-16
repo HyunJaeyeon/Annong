f92a4550ca6a958427dde9c85386d182227b310c
HyunJaeyeon <bomul0109@gmail.com>
2024-06-17 02:42:28 +0900

    Feat: 우편함 List에 저장된 타이틀 띄우기

--- Annong/MessageBoxView.swift
+++ Annong/MessageBoxView.swift
@@ -6,6 +6,8 @@ struct MessageBoxView: View {
     @Binding var myNickname: String
     @State private var isShownFullScreenCover = false
     
+    @Query private var posts: [Post]
+    
     var body: some View {
         VStack(alignment: .leading, spacing: 0) {
             Text("💎 \(myNickname)의 우편함")
@@ -15,8 +17,9 @@ struct MessageBoxView: View {
                 .padding(.leading)
             
             List {
-                Text("나 스티브잡스인데, 이 앱 좋다")
-                Text("나 조너선 아이브인데, 이 앱 이쁘다")
+                ForEach(posts) { post in
+                    Text("\(post.title)")
+                }
             }
         }
         .padding(.top)
@@ -37,9 +40,4 @@ struct MessageBoxView: View {
         }
         .padding(.bottom)
     }
-}
-
-#Preview {
-    MessageBoxView(myNickname: .constant("젠예"))
-        .preferredColorScheme(.dark)
 }

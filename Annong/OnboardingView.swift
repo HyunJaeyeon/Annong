396ab1bc0943048680acfa349969077a6c527145
HyunJaeyeon <bomul0109@gmail.com>
2024-06-16 19:18:38 +0900

    Feat: 닉네임 입력 textfield 구현

--- Annong/OnboardingView.swift
+++ Annong/OnboardingView.swift
@@ -5,4 +5,29 @@
 //  Created by Hyun Jaeyeon on 6/14/24.
 //
 
-import Foundation
+import SwiftUI
+
+struct OnboardingView: View {
+    
+    @State var nickname = ""
+    
+    var body: some View {
+        VStack(alignment: .leading){
+            Text("안농?\n닉네임을 입력해주세요")
+                .font(.title)
+                .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                .foregroundStyle(.white)
+            
+            TextField("이름을 입력해주세요", text: $nickname)
+                .padding()
+                .background(Color.secondary.opacity(0.15))
+                .clipShape(Capsule())
+                .frame(width: 361)
+        }
+    }
+}
+
+#Preview {
+    OnboardingView()
+        .preferredColorScheme(.dark)
+}

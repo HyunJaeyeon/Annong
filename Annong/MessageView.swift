dcba6e4f7f94da5636240feb627cc531f3055e3e
HyunJaeyeon <bomul0109@gmail.com>
2024-07-02 17:56:55 +0900

    fix: 머지 수정

--- Annong/MessageView.swift
+++ Annong/MessageView.swift
@@ -30,19 +30,11 @@ struct MessageView: View {
                     .foregroundStyle(.accent)
                     .padding(.top, 26)
                 
-<<<<<<< HEAD
-//                Image(uiImage: uiImage)
-//                    .resizable()
-//                    .scaledToFit()
-//                    .frame(height: 200)
-//                    .cornerRadius(10)
-=======
                 Image("")
                     .resizable()
                     .scaledToFit()
                     .frame(height: 200)
                     .cornerRadius(10)
->>>>>>> origin/main
                 
                 // 내용 입력 부분
                 Text("내용")
